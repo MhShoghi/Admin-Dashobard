6356e7c8eb0aa2560e42fff2bc9c753891b2052e
Mohammad Hosein Shoghi <hoseinshoghi@gmail.com>
2022-08-28 16:41:41 +0430

    Implemented Datatable and edit some props in chart component and complete Single component

--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -1,7 +1,60 @@
+import Chart from "../../components/chart/Chart";
+import Navbar from "../../components/navbar/Navbar";
+import Sidebar from "../../components/sidebar/Sidebar";
 import "./single.scss";
 
 const Single = () => {
-  return <div>Single</div>;
+  return (
+    <div className="single">
+      <Sidebar />
+
+      <div className="singleContainer">
+        <Navbar />
+
+        <div className="top">
+          <div className="left">
+            <div className="editButton">Edit</div>
+            <h1 className="title">Information</h1>
+            <div className="item">
+              <img
+                src="https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg"
+                alt=""
+                className="itemImg"
+              />
+              <div className="details">
+                <h1 className="itemTitle">Jane Done</h1>
+                <div className="detailItem">
+                  <span className="itemKey">Email:</span>
+                  <span className="itemValue">JaneDone@gmail.com</span>
+                </div>
+
+                <div className="detailItem">
+                  <span className="itemKey">Phone:</span>
+                  <span className="itemValue">+989139740329</span>
+                </div>
+
+                <div className="detailItem">
+                  <span className="itemKey">Address:</span>
+                  <span className="itemValue">
+                    Khordad Alley, Mobarake, Isfahan
+                  </span>
+                </div>
+
+                <div className="detailItem">
+                  <span className="itemKey">Country:</span>
+                  <span className="itemValue">IRAN</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="right">
+            <Chart aspect={3 / 1} title="User Spending ( Last 6 Months )" />
+          </div>
+        </div>
+        <div className="bottom"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Single;
